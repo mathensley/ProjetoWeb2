5bc4d160eeea2be8c00c89be2823ed142ad268bc
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2025-01-25 10:35:24 -0300

    feat: Adicionando Sw documentação Swaggeer ao projeto

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express, {Express, Request, Response } from "express";
 import testeRouter from "./routes/RouterController.js";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "./swagger.json";
 
 const app: Express  = express();
 const port: Number  = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson))
+
 
 app.use(testeRouter);
+
 
 app.get("/", (request: Request, response: Response) => {
     response.send("Hello World!");
@@ -16,4 +21,5 @@ app.get("/", (request: Request, response: Response) => {
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Swagger rodando em http://localhost:${port}/api-docs`);
 });
