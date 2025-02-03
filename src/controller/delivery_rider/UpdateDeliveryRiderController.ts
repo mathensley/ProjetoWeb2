16d7a0f8bd212b0e69bd8b1e7a375a520bebff4b
mathensley <matheushensley_25@hotmail.com>
2025-02-02 21:33:46 -0300

    feat: mais endpoints de entregador

---
+++ src/controller/delivery_rider/UpdateDeliveryRiderController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { DeliveryRider } from "@prisma/client";
+import { UpdateDeliveryRiderService } from "../../service/delivery_rider/UpdateDeliveryRiderService.js";
+
+export class UpdateDeliveryRiderController {
+    private updateDeliveryRiderService: UpdateDeliveryRiderService;
+
+    constructor() {
+        this.updateDeliveryRiderService = new UpdateDeliveryRiderService();
+    }
+
+    async handle(request: Request, response: Response) {
+        try {
+            const { id } = request.params;
+            const updateData: DeliveryRider = request.body;
+
+            const updatedRider = await this.updateDeliveryRiderService.update(id, updateData);
+            return response.status(200).json(updatedRider);
+        } catch (error) {
+            if (error instanceof Error) {
+                return response.status(500).json({
+                    error: "An expected error ocurred.", 
+                    info: error.message,
+                    stackTrace: error.stack
+                });
+            }
+        }
+    }
+}
--- src/route/delivery_rider/deliveryRoutes.ts
+++ src/route/delivery_rider/deliveryRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { Request, Response } from "express";
 import { CreateDeliveryRiderController } from "../../controller/delivery_rider/CreateDeliveryRiderController.js";
 import { GetDeliveryRiderController } from "../../controller/delivery_rider/GetDeliveryRiderController.js";
+import { GetDeliveryRiderByIdController } from "../../controller/delivery_rider/GetDeliveryRiderByIdController.js";
+import { UpdateDeliveryRiderController } from "../../controller/delivery_rider/UpdateDeliveryRiderController.js";
 import { DeleteDeliveryRiderController } from "../../controller/delivery_rider/DeleteDeliveryRiderController.js";
 import { AuthService } from "../../service/auth/AuthService.js";
 
@@ -9,10 +11,14 @@ const deliveryRoutes = Router();
 
 const createDeliveryRiderController = new CreateDeliveryRiderController();
 const getDeliveryRiderController = new GetDeliveryRiderController();
+const getDeliveryRiderByIdController = new GetDeliveryRiderByIdController();
+const updateDeliveryRiderController = new UpdateDeliveryRiderController();
 const deleteDeliveryRiderController =  new DeleteDeliveryRiderController();
 
 deliveryRoutes.post("/api/delivery_riders", (request: Request, response: Response) => {createDeliveryRiderController.handle(request, response)});
 deliveryRoutes.get("/api/delivery_riders", (request: Request, response: Response) => {getDeliveryRiderController.handle(request, response)});
+deliveryRoutes.get("/api/delivery_riders/:id", (request: Request, response: Response) => {getDeliveryRiderByIdController.handle(request, response)});
+deliveryRoutes.put("/api/delivery_riders/:id", (request: Request, response: Response) => {updateDeliveryRiderController.handle(request, response)})
 deliveryRoutes.delete("/api/delivery_riders/:id", (request: Request, response: Response) => {deleteDeliveryRiderController.handle(request, response)});
 
 export default deliveryRoutes;
