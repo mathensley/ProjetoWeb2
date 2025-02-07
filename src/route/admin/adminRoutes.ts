df7f1828631b47c24bd798003e0415e993f3deac
mathensley <matheushensley_25@hotmail.com>
2025-02-06 22:00:22 -0300

    feat: adicionando endpoints de get e delete para admin

--- src/route/admin/adminRoutes.ts
+++ src/route/admin/adminRoutes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { Request, Response } from "express";
 import { CreateAdminController } from '../../controller/admin/CreateAdminController.js';
+import { GetAdminController } from '../../controller/admin/GetAdminController.js';
 import { DeleteAdminController } from '../../controller/admin/DeleteAdminController.js';
 
 const adminRoutes = Router();
 
 const createAdminController = new CreateAdminController();
+const getAdminController = new GetAdminController();
 const deleteAdminController =  new DeleteAdminController();
 
 adminRoutes.post("/api/admins", (request: Request, response: Response) => {createAdminController.handle(request, response)});
+adminRoutes.get("/api/admins", (request: Request, response: Response) => {getAdminController.handle(request, response)});
 adminRoutes.delete("/api/admins/:id", (request: Request, response: Response) => {deleteAdminController.handle(request, response)});
 
 export default adminRoutes;
