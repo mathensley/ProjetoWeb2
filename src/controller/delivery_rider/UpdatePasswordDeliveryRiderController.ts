a0629784f0606c3724a50ea13c2ebb2a469dfe34
mathensley <matheushensley_25@hotmail.com>
2025-02-09 19:18:49 -0300

    feat: adicionando um patch para entregador

---
+++ src/controller/delivery_rider/UpdatePasswordDeliveryRiderController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { UpdatePasswordDeliveryRiderService } from "../../service/delivery_rider/UpdatePasswordDeliveryRiderService.js";
+
+export class UpdatePasswordDeliveryRiderController {
+    private updatePasswordDeliveryRiderService: UpdatePasswordDeliveryRiderService;
+
+    constructor() {
+        this.updatePasswordDeliveryRiderService = new UpdatePasswordDeliveryRiderService();
+    }
+
+    async handle(request: Request, response: Response) {
+        try {
+            const { id } = request.params;
+            const { password } = request.body;
+
+            const updatedRider = await this.updatePasswordDeliveryRiderService.update(String(id), String(password));
+            return response.status(200).json(updatedRider);
+        } catch (error) {
+            if (error instanceof Error) {
+                return response.status(500).json({
+                    error: "An expected error ocurred.", 
+                    info: error.message,
+                    stackTrace: error.stack
+                });
+            }
+        }
+    } 
+}
--- src/route/delivery_rider/deliveryRoutes.ts
+++ src/route/delivery_rider/deliveryRoutes.ts
@@ -3,8 +3,9 @@ import { Request, Response } from "express";
 import { CreateDeliveryRiderController } from "../../controller/delivery_rider/CreateDeliveryRiderController.js";
 import { GetDeliveryRiderController } from "../../controller/delivery_rider/GetDeliveryRiderController.js";
 import { GetDeliveryRiderByIdController } from "../../controller/delivery_rider/GetDeliveryRiderByIdController.js";
-import { UpdateDeliveryRiderController } from "../../controller/delivery_rider/UpdateDeliveryRiderController.js";
 import { DeleteDeliveryRiderController } from "../../controller/delivery_rider/DeleteDeliveryRiderController.js";
+import { UpdateDeliveryRiderController } from "../../controller/delivery_rider/UpdateDeliveryRiderController.js";
+import { UpdatePasswordDeliveryRiderController } from "../../controller/delivery_rider/UpdatePasswordDeliveryRiderController.js";
 import { AuthService } from "../../service/auth/AuthService.js";
 
 const deliveryRoutes = Router();
@@ -12,13 +13,15 @@ const deliveryRoutes = Router();
 const createDeliveryRiderController = new CreateDeliveryRiderController();
 const getDeliveryRiderController = new GetDeliveryRiderController();
 const getDeliveryRiderByIdController = new GetDeliveryRiderByIdController();
-const updateDeliveryRiderController = new UpdateDeliveryRiderController();
 const deleteDeliveryRiderController =  new DeleteDeliveryRiderController();
+const updateDeliveryRiderController = new UpdateDeliveryRiderController();
+const updatePasswordDeliveryRiderController = new UpdatePasswordDeliveryRiderController();
 
 deliveryRoutes.post("/api/delivery_riders", (request: Request, response: Response) => {createDeliveryRiderController.handle(request, response)});
 deliveryRoutes.get("/api/delivery_riders", (request: Request, response: Response) => {getDeliveryRiderController.handle(request, response)});
 deliveryRoutes.get("/api/delivery_riders/:id", (request: Request, response: Response) => {getDeliveryRiderByIdController.handle(request, response)});
-deliveryRoutes.put("/api/delivery_riders/:id", (request: Request, response: Response) => {updateDeliveryRiderController.handle(request, response)})
 deliveryRoutes.delete("/api/delivery_riders/:id", (request: Request, response: Response) => {deleteDeliveryRiderController.handle(request, response)});
+deliveryRoutes.put("/api/delivery_riders/:id", (request: Request, response: Response) => {updateDeliveryRiderController.handle(request, response)});
+deliveryRoutes.patch("/api/delivery_riders/password/:id", (request: Request, response: Response) => {updatePasswordDeliveryRiderController.handle(request, response)});
 
 export default deliveryRoutes;
